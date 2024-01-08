675f111943a59bd7c287cc0b7a6fef7c6e96a0fc
Arthur Cai <arthurca3607@gmail.com>
2024-01-07 19:55:02 -0600

    codebyter

---
+++ bot.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import discord
+
+BOT_TOKEN = "MTE5MzcwOTIzMjAwNTA2NjgxNA.GrBXNp.unpKH7AH1cAjtgWnDPSn05XqpEBiGXVDyuOIx8"
+CHANNEL_ID = 1141507451259211839
+
+bot = commands.Bot(command_prefix="~", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Hello")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("codebyter")
+
+bot.run(BOT_TOKEN)
