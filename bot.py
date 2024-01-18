fafe763fa90ac9747b6c63f727771b73c275184c
Arthur Cai <arthurca3607@gmail.com>
2024-01-17 21:42:43 -0600

    con

--- bot.py
+++ bot.py
@@ -4,20 +4,18 @@ import discord
 import io, asyncio
 import aiohttp
 import time
-
-BOT_TOKEN = "MTE5MzcwOTIzMjAwNTA2NjgxNA.GrBXNp.unpKH7AH1cAjtgWnDPSn05XqpEBiGXVDyuOIx8"
-CHANNEL_ID = 1141507451259211839
+import config
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("Logged in as:", bot.user.name)
-    channel = bot.get_channel(CHANNEL_ID)
+    channel = bot.get_channel(config.CHANNEL_ID)
 
 @bot.command(pass_context=True)
 async def picture(ctx, message, file):
-    channel = bot.get_channel(CHANNEL_ID) #Whatever channel you want to send image to
+    channel = bot.get_channel(config.CHANNEL_ID) #Whatever channel you want to send image to
     async with aiohttp.ClientSession() as session:
         async with session.get(file) as resp:
             img = await resp.read()
@@ -25,4 +23,4 @@ async def picture(ctx, message, file):
                 await channel.send(message)
                 await channel.send(file=discord.File(file, "image.png"))
 
-bot.run(BOT_TOKEN)
+bot.run(config.BOT_TOKEN)
