a4743a8b1ef22444f3211d5548d616e721c487c7
Arthur Cai <arthurca3607@gmail.com>
2024-01-20 13:24:19 -0600

    working on spictyure

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from discord.ext import commands
 import discord
-import io, asyncio
-import aiohttp
-import datetime
+import io, asyncio, os
+import aiohttp, typing
+import datetime, pathlib
 import config
+import requests
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,17 +21,27 @@ async def on_ready():
     print("Logged in as:", bot.user.name)
 
 @bot.command(pass_context=True)
-async def picture(ctx, file, *, message):
+async def upload(ctx, attachment: discord.Attachment,* , message):
+    file = attachment.url
     channel = bot.get_channel(config.CHANNEL_ID) #whatever channel you want to send image to
     async with aiohttp.ClientSession() as session:
         async with session.get(file) as resp:
             img = await resp.read()
             with io.BytesIO(img) as file:
                 await channel.send(message)
-                await channel.send(file=discord.File(file, "image.png"))
+                await channel.send(file=discord.File(file, "image.gif"))
 
 @bot.command(pass_context=True)
 async def schedule(ctx, t, min, *, message):
+    channel = bot.get_channel(config.CHANNEL_ID)
+    #delay of 30 seconds, not quite accurate but it does schedule
+    while True:
+        if timer().hour == int(t) and timer().minute == int(min):
+            await channel.send(message)
+        await asyncio.sleep(60)
+
+@bot.command(pass_context=True)
+async def spicture(ctx, t, min, attachment: discord.Attachment, *, message):
     channel = bot.get_channel(config.CHANNEL_ID)
     #delay of 30 seconds, not quite accurate but it does schedule
     while True:
